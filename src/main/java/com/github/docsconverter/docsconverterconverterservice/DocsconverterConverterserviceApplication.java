20a2242e6cadab313ad45d6979db4b058d6e76b6
vitalijbukatkin <vitaliy.bukatkin@gmail.com>
2020-02-18 11:40:00 +0300

    Init repo

---
+++ src/main/java/com/github/docsconverter/docsconverterconverterservice/DocsconverterConverterserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.docsconverter.docsconverterconverterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsconverterConverterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsconverterConverterserviceApplication.class, args);
+	}
+
+}
